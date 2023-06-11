d4c4ac84c07bb54f24d72e994ab595a7ae1e6300
anikdev2016 <anikdev2016@gmail.com>
2023-06-11 15:18:59 +0800

    [DONE] set up completed, mongoose, express, cors, parse

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,22 @@
-import express, { Application } from "express";
+import express, {
+  Application,
+  Request,
+  Response,
+  request,
+  response,
+  urlencoded,
+} from "express";
+import cors from "cors";
 const app: Application = express();
 
+// middleware
+app.use(cors());
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// primary route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello university management");
+});
 export default app;
