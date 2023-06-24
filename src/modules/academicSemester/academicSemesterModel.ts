3e9aa39b8c15a7955a16b7ea90f35214bf8c8db3
anikdev2016 <anikdev2016@gmail.com>
2023-06-24 13:35:52 +0800

    blocked same data entry for semester in same year

--- src/modules/academicSemester/academicSemesterModel.ts
+++ src/modules/academicSemester/academicSemesterModel.ts
@@ -1,4 +1,6 @@
+import status from "http-status";
 import { Schema, model } from "mongoose";
+import ApiError from "../../errors/ApiError";
 import {
   academicSemesterConstantCode,
   academicSemesterConstantMonth,
@@ -42,6 +44,29 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   }
 );
+
+//? 2025 autumm
+//! 2025 autumn X
+//? 2026 autumn
+//? same year && same semester --> duplicate entry (it won't happen)
+
+// ! handling same year and same semester issue
+//  data --> check --> same year and same semester
+// ! Pre hook
+academicSemesterSchema.pre("save", async function (next) {
+  // checking with pre hook, before saving the data in database, that whether the data which is being saved from the instance of the schema. here, this refers to the current instance of the schema that is about to be saved in the database.
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+  if (isExist) {
+    throw new ApiError(
+      status.CONFLICT,
+      "Semester already exists for the same year."
+    );
+  }
+  next();
+});
 // create model
 const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   "AcademicSemester",
